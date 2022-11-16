dba35c2ce383b0449cb6fb6f49b8446ae94d88b2
Lucas Mendes <luckasmendys72@hotmail.com>
2022-11-16 11:02:08 -0300

    adicionando a função soma à calculadora

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const teclas = document.querySelectorAll('.teclas');
 const telaDaCalculadora = document.getElementById('visor');
+const telaDeArmazenamento = document.getElementById('armazenamento');
 const teclaApagar = document.querySelector('[data-apagar]');
 const teclaSoma = document.querySelector('[data-soma]');
+const teclaIgual = document.querySelector('[data-igual]');
+var primeiroNumero = parseInt(telaDaCalculadora.value);
+var segundoNumero = parseInt(telaDeArmazenamento.value);
 
 teclaApagar.addEventListener('click', () => {
-    telaDaCalculadora.value = ""
+    apagarTela();
 })
+
+function apagarTela(){
+    telaDaCalculadora.value = ""
+    telaDeArmazenamento.value = ""
+}
 
 teclas.forEach(element => {
     element.addEventListener('click', (evento) => {
@@ -17,6 +26,26 @@ teclas.forEach(element => {
 function mostraValorNaTela(valorClicado) {
     telaDaCalculadora.value += valorClicado;    
 }
+
+teclaSoma.addEventListener('click', () => funcaoSoma())
+
+function funcaoSoma(){
+    telaDeArmazenamento.value = parseInt(telaDaCalculadora.value);
+    telaDaCalculadora.value = ""
+}
+
+teclaIgual.addEventListener('click', () => {
+    funcaoIgualdade();
+})
+
+function funcaoIgualdade(){
+    var somou = parseInt(telaDaCalculadora.value) + parseInt(telaDeArmazenamento.value)
+    apagarTela();
+    telaDaCalculadora.value = somou;
+}
+
+
+
 
 
 
