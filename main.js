394428858446cd27ee8237266762c9a381316070
Lucas Mendes <luckasmendys72@hotmail.com>
2022-11-16 17:05:33 -0300

    Adicionando as funcionalidades de subtrair-multiplicar-dividir

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const telaMostraOperacao = document.getElementById('operacao');
 const teclaApagar = document.querySelector('[data-apagar]');
 const teclaSoma = document.querySelector('[data-soma]');
 const teclaSubtrai = document.querySelector('[data-subtrai]');
+const teclaMultiplica = document.querySelector('[data-multiplica]');
+const teclaDivide = document.querySelector('[data-divide]');
 const teclaIgual = document.querySelector('[data-igual]');
 
 teclaApagar.addEventListener('click', () => {
@@ -44,6 +46,23 @@ function funcaoSubtrai(){
     telaMostraOperacao.value = "-"
 }
 
+teclaMultiplica.addEventListener('click', () => funcaoMultiplica())
+
+function funcaoMultiplica(){
+    telaDeArmazenamento.value = parseInt(telaDaCalculadora.value);
+    telaDaCalculadora.value = ""
+    telaMostraOperacao.value = "x"
+}
+//Função Multiplicar
+
+teclaDivide.addEventListener('click', () => funcaoDivide())
+
+function funcaoDivide(){
+    telaDeArmazenamento.value = parseInt(telaDaCalculadora.value);
+    telaDaCalculadora.value = ""
+    telaMostraOperacao.value = "/"
+}
+
 teclaIgual.addEventListener('click', () => {
     funcaoIgualdade();
 })
@@ -51,11 +70,17 @@ teclaIgual.addEventListener('click', () => {
 function funcaoIgualdade(){
     var somou = parseInt(telaDeArmazenamento.value) + parseInt(telaDaCalculadora.value)
     var subtraiu = parseInt(telaDeArmazenamento.value) - parseInt(telaDaCalculadora.value)
+    var multiplicou = parseInt(telaDeArmazenamento.value) * parseInt(telaDaCalculadora.value)
+    var dividiu = parseInt(telaDeArmazenamento.value) / parseInt(telaDaCalculadora.value)
 
     if(telaMostraOperacao.value == '+'){
         telaDaCalculadora.value = somou;
     }else if(telaMostraOperacao.value == '-'){
         telaDaCalculadora.value = subtraiu;
+    }else if(telaMostraOperacao.value == 'x'){
+        telaDaCalculadora.value = multiplicou;
+    }else{
+        telaDaCalculadora.value = dividiu;
     }
 
     telaDeArmazenamento.value = ""
